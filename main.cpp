76083fa3a805cb30e7133c89c7eb0dd67dc2b103
王辰毓 <margarita@MacBook-Pro-3.local>
2024-11-06 22:01:55 -0800

    2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,67 @@
 #include <iostream>
+/*
+economy fuction
+int econ(){
+int rate;
+-10 to +10%
+return rate;}
+*/
+
+/*
+ policy(){
+3% chance for the market to close job for international students;
+tax benifits for hiring
+
+}*/
+
+/*
+tech(){
+if new product
+else if newtechnology
+else sale decrease;
+
+}
+*/
 
 int mian(){
+/*
+creat the map
+map<string, array<list<int>, 3>> jobMarket;
+jobMarket["Orange"] = {
+list<int>{100, 120}, 
+list<int>{50, 60},    
+list<int>{20, 30}     
+};
 
+jobMarket["MacroH"] = {
+list<int>{80, 90},    
+list<int>{40, 50},    
+list<int>{10, 20}     
+};
+
+jobMarket["Booble"] = {
+list<int>{70, 75},    
+list<int>{60, 65},   
+list<int>{25, 30}   
+};
+
+jobMarket["CompanyC"] = {
+list<int>{50, 55},   
+list<int>{70, 80},    
+list<int>{15, 20}     
+};
+
+jobMarket["stardown"] = {
+list<int>{90, 100},   
+list<int>{45, 50},   
+list<int>{5, 10}      
+};
+
+run 36 times;
+
+
+
+
+*/
 return 0;
 }
