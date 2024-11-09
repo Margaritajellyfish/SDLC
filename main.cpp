39ba243138644ecd5cfb03a74f601c1e5398d79a
王辰毓 <margarita@MacBook-Pro-3.local>
2024-11-08 22:36:03 -0800

    6

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 #include <map>
 #include <array>
 #include <list>
+#include <vector>
+#include <cmath>  
+
+using namespace std;
 
 int econ() {
     int rate = rand() % 21 - 10;  
@@ -10,7 +14,7 @@ int econ() {
 }
 
 
-void populateRandomJobs(const vector<string>& joblist) {
+void runRanJobs( vector<string> joblist, list<string> cateegrory) {
 
     srand(static_cast<unsigned int>(time(0)));
 
@@ -19,21 +23,12 @@ void populateRandomJobs(const vector<string>& joblist) {
 
     for (int i = 0; i < numJobs; ++i) {
         int randomIndex = rand() % joblist.size();
-        jobList.push_back(joblist[randomIndex]);
+        cateegrory.push_back(joblist[randomIndex]);
     }
-
-    map<string, int> jobCount;
-    for (const auto& job : jobList) {
-        jobCount[job]++;
-    }
-
-    cout << "Job Counts:" << std::endl;
-    for (const auto& entry : jobCount) {
-        cout << entry.first << ": " << entry.second << endl;
-    }
+   
 }
 
-void policy(map<string, array<list<int>, 3>>& jobMarket,int rate) {
+void policy(map<string, array<list<int>, 3>> & jobMarket,int rate) {
     int randomChance = rand() % 100;
     randomChance = randomChance + rate;
     if (randomChance < 3) {
@@ -47,11 +42,10 @@ void policy(map<string, array<list<int>, 3>>& jobMarket,int rate) {
         
         cout << "Policy: Tax benefits for hiring more employees.\n";
         for (auto& company : jobMarket) {
-            for (auto& jobList : company.second) {
-                for (auto& job : jobList) {
+
+                for (auto& job : company) {
                     job += 5; 
                 }
-            }
         }
     }
 
@@ -95,34 +89,49 @@ int mian(){
 
 map<string, array<list<int>, 3>> jobMarket;
 jobMarket["Orange"] = {
-list<int>{100, 120}, 
-list<int>{50, 60},    
-list<int>{20, 30}     
+list<string> (), 
+list<string> (),    
+list<string> ()   
 };
+runRanJobs(jobList, jobMarket["Orange"][0]);
+runRanJobs(jobList, jobMarket["Orange"][1]); 
+runRanJobs(jobList, jobMarket["Orange"][2]);
 
 jobMarket["MacroH"] = {
-list<int>{80, 90},    
-list<int>{40, 50},    
-list<int>{10, 20}     
+list<string> (), 
+list<string> (),    
+list<string> ()     
 };
+runRanJobs(jobList, jobMarket["MacroH"][0]);
+runRanJobs(jobList, jobMarket["MacroH"][1]); 
+runRanJobs(jobList, jobMarket["MacroH"][2]);
 
 jobMarket["Booble"] = {
-list<int>{70, 75},    
-list<int>{60, 65},   
-list<int>{25, 30}   
+list<string> (), 
+list<string> (),    
+list<string> ()   
 };
+runRanJobs(jobList, jobMarket["Booble"][0]);
+runRanJobs(jobList, jobMarket["Booble"][1]); 
+runRanJobs(jobList, jobMarket["Booble"][2]);
 
 jobMarket["CompanyC"] = {
-list<int>{50, 55},   
-list<int>{70, 80},    
-list<int>{15, 20}     
+list<string> (), 
+list<string> (),    
+list<string> ()     
 };
+runRanJobs(jobList, jobMarket["CompanyC"][0]);
+runRanJobs(jobList, jobMarket["CompanyC"][1]);
+runRanJobs(jobList, jobMarket["CompanyC"][2]);
 
 jobMarket["stardown"] = {
-list<int>{90, 100},   
-list<int>{45, 50},   
-list<int>{5, 10}      
+list<string> (), 
+list<string> (),    
+list<string> ()      
 };
+runRanJobs(jobList, jobMarket["stardown"][0]);
+runRanJobs(jobList, jobMarket["stardown"][1]);
+runRanJobs(jobList, jobMarket["stardown"][2]);
 
 
  for (int i = 0; i < 36; ++i) {
@@ -148,6 +157,7 @@ list<int>{5, 10}
             }
         }
  }
-
-return 0;
+      return 0;
 }
+
+
